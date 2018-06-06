630bc19b503cb39281aadf0ef4c92c932223e8bd
Alejandro Mostajo <amostajo@gmail.com>
2018-06-05 18:31:05 -0600

    Fixes missing exception error.

--- src/Engine.php
+++ src/Engine.php
@@ -2,6 +2,8 @@
 
 namespace WPMVC\MVC;
 
+use Exception;
+
 /**
  * MVC engine.
  *
@@ -9,7 +11,7 @@ namespace WPMVC\MVC;
  * @copyright 10Quality <http://www.10quality.com>
  * @license MIT
  * @package WPMVC\MVC
- * @version 2.0.0
+ * @version 2.1.4
  */
 class Engine
 {
